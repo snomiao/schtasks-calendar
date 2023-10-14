cf22632dcab872a721904d8f8499c699870dfbb6
snomiao <snomiao@gmail.com>
2023-10-15 07:40:31 +0900

    fix(schcal): bump
    bump

--- release.config.cjs
+++ release.config.cjs
@@ -2,5 +2,5 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 module.exports = {
-  branches: ['+([0-9])?(.{+([0-9]),x}).x', 'main', 'next', 'next-major', {name: 'beta', prerelease: true}, {name: 'alpha', prerelease: true}]
+  branches: ['+([0-9])?(.{+([0-9]),x}).x', 'main', 'next', 'next-major', {name: 'beta', prerelease: true}, {name: 'alpha', prerelease: true}],
 };
